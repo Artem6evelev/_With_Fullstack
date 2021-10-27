28cfd8b6fe73863121c70caa320d1eeade23b78a
Артем Шевелев <artemsevelev@Artems-MacBook-Pro.local>
2021-10-27 09:21:01 +0300

    format data from two endpoint into one template with the same format

--- interview-backend-master/src/controllers/images.controller.js
+++ interview-backend-master/src/controllers/images.controller.js
@@ -1,12 +1,38 @@
-const catchAsync = require('../utils/catchAsync');
+const catchAsync = require("../utils/catchAsync");
+
+const photosUrl =
+  "https://my-json-server.typicode.com/coing-dev/photo-api/photos";
+
+const imagesUrl =
+  "https://my-json-server.typicode.com/coing-dev/photo-api/images";
+
+function format(resImg, resPhoto) {
+  let data = [];
+  resImg.data[0].forEach((picture) => {
+    data.push(picture);
+  });
+  resPhoto.data[0].forEach((picture) => {
+    template = {
+      albumId: picture.id,
+      id: picture.id,
+      title: picture.title,
+      path: picture.url,
+    };
+    data.push(template);
+  });
+  return {
+      status: 1,
+      data: data
+  }
+}
 
 const getImages = catchAsync(async (req, res) => {
-    /**
-     * TODO CALL TO OUTSIDE API
-     * AND RETURN THE VALID RESPONSE
-     */
+  /**
+   * TODO CALL TO OUTSIDE API
+   * AND RETURN THE VALID RESPONSE
+   */
 });
 
 module.exports = {
-    getImages
+  getImages,
 };
